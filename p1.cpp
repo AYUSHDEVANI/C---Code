fd1cc0f7410f74795a28969b373069a6de965550
AYUSH DEVANI <108976599+AYUSHDEVANI@users.noreply.github.com>
2023-02-18 18:25:20 +0530

    first commit

---
+++ p1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int a=18,b=17;
+    int sum = a + b;
+    cout<<"Hello World\n";
+    std::cout<<"Hello World"<<endl;
+    cout<<"Hello World"<<endl;
+
+    cout<<a;
+    cout<<"\nI am "<<a<<" year old\n";
+
+    cout<<sum;
+
+    return 0;
+}
