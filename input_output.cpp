d5fefe48d484af4b624efe556ce37586e8e08043
AYUSH DEVANI <108976599+AYUSHDEVANI@users.noreply.github.com>
2023-02-18 18:46:59 +0530

    C Code

---
+++ input_output.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter num1 value : ";
+    cin>>num1;
+    // '<<' is called insertion operator
+
+    cout<<"Enter num2 value : ";
+    cin>>num2;
+    // '>>' is called Extraction operator
+
+    cout<<"The sum is "<<num1+num2;
+
+    return 0;
+}
