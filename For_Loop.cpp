e4d5c5a6937f3d6cd62c34db9700295147d2da90
AYUSH DEVANI <108976599+AYUSHDEVANI@users.noreply.github.com>
2023-02-22 08:15:03 +0530

    For Loop in C++

---
+++ For_Loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    /* Syntax 
+        for(initilization; condition; updating)
+        {
+            Loop body;
+        }
+    */
+
+    for (int i = 1; i <= 40; i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
