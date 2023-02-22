44dfcfa93e0b9515514ee1d487b26ee7b99a4fa8
AYUSH DEVANI <108976599+AYUSHDEVANI@users.noreply.github.com>
2023-02-22 08:15:38 +0530

    Manipulator in C++

--- manipulator.cpp
+++ manipulator.cpp
@@ -10,10 +10,10 @@ int main()
     cout << "The value of c without manipulator is : " << c << endl;
     cout << "The value of d without manipulator is : " << d << endl;
 
-    cout << "The value of a with manipulator is : "<<setw(4) << a << endl;
-    cout << "The value of b with manipulator is : "<<setw(4) << b << endl;
-    cout << "The value of c with manipulator is : "<<setw(4) << c << endl;
-    cout << "The value of d with manipulator is : "<<setw(4) << d << endl;
+    cout << "The value of a with manipulator is : " << setw(4) << a << endl;
+    cout << "The value of b with manipulator is : " << setw(4) << b << endl;
+    cout << "The value of c with manipulator is : " << setw(4) << c << endl;
+    cout << "The value of d with manipulator is : " << setw(4) << d << endl;
 
     return 0;
 }
